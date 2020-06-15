3040cf5ef3334406f24b30d73ad77611b4dbca75
FASfalcon <alexanderfedasov@gmail.com>
2020-06-15 17:49:15 +0300

    HCG. update1.0 - test

---
+++ src/com/dragon/HotColdGame.java
@@ -0,0 +1,32 @@
+package src.com.dragon;
+
+import java.util.Scanner;
+
+/*
+ */
+
+public class HotColdGame {
+
+    private static int rnd(int maxNum) {
+        return (int) (Math.random() * ++maxNum);
+    }
+
+    public static void main(String[] args) {
+
+        int maxNum = 100;
+        int rndNum = rnd(maxNum);
+        System.out.println(rndNum);
+
+        System.out.println("Ведьмак уже загадал число! \nТвой ход!");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (rndNum > number) {
+            System.out.println("Бери больше!");
+        } else if (rndNum < number) {
+            System.out.println("Бери меньше!");
+        } else {
+            System.out.println("Ты - Ведьмак!");
+        }
+    }
+}
