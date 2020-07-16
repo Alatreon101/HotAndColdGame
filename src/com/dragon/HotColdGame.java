7f2aad36cae4e858c38931ac5539dd6efbd839db
FASfalcon <alexanderfedasov@gmail.com>
2020-07-16 16:42:19 +0300

    HCG. update2.0 - изменен функционал

--- src/com/dragon/HotColdGame.java
+++ src/com/dragon/HotColdGame.java
@@ -7,28 +7,60 @@ import java.util.Scanner;
 
 public class HotColdGame {
 
-    private static int rnd(int maxNum) {
-        return (int) (Math.random() * ++maxNum);
-    }
+    final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        new HotColdGame().guessNumber();
+    }
 
-        boolean yn;
+    public void guessNumber() {
+        int rnd = (int) (Math.random() * 100) + 1;
+        guessNumber(rnd);
+    }
 
-        int maxNum = 100;
-        int rndNum = rnd(maxNum);
-        System.out.println(rndNum);
+    public void guessNumber(int numToGuess) {
+        int supposeNum;
+        do {
+            supposeNum = askNum("Попробуйте угадать случайное число. Введите Ваше число или введите \"exit\" для выхода ");
+            System.out.println(hotColdMessage(numToGuess, supposeNum));
+        } while (numToGuess != supposeNum);
+    }
 
-        System.out.println("Ведьмак уже загадал число! \nТвой ход!");
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
+    private String hotColdMessage(int numToGuess, int supposeNum) {
+        final int diff = Math.abs(numToGuess - supposeNum);
 
-        if (rndNum > number) {
-            System.out.println("Бери больше!");
-        } else if (rndNum < number) {
-            System.out.println("Бери меньше!");
-        } else {
-            System.out.println("Ты - Ведьмак!");
+        if (diff == 0)
+            return "Угадал";
+        if (diff < 5) {
+            return "Огонь!";
         }
+        if ((diff > 5) && (diff <= 10)) {
+            return "Горячо";
+        }
+        return "Холодно";
+    }
+
+    public int askNum(String message) {
+        System.out.println(message);
+        String userInput;
+        do {
+            userInput = sc.next();
+            if ("exit".equals(userInput)) {
+                System.exit(0);
+            }
+            if (!isNumber(userInput)) {
+                System.out.println("Ошибка ввода! Попытайся снова!");
+            }
+        } while (!isNumber(userInput));
+        return Integer.parseInt(userInput);
+    }
+
+    private boolean isNumber(String supposedStr) {
+        try {
+            Integer.parseInt(supposedStr);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 }
